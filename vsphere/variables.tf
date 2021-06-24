141d8e9213eb34970ce2cb55bd35c700a9ba5a15
Khabir Uddin <khuddin@csc.fi>
2021-06-24 17:03:19 +0300

    updated info in variables

--- vsphere/variables.tf
+++ vsphere/variables.tf
@@ -1,1 +1,19 @@
 # variables will come here
+
+variable "vsphere_server" {
+  description = "vsphere server info"
+  type = "string"
+  default = "192.168.20.142"
+}
+
+variable "vsphere_user" {
+  description = "administrative user"
+  type = "string"
+  default = "root"
+}
+
+variable "vsphere_password" {
+  description = "administrative user password"
+  type = "string"
+  default = "YourPasswordH3r3"
+}
