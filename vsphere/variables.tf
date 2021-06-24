aa09bd1748e797f28afe2f5c864f1d1a5849610b
Khabir Uddin <khuddin@csc.fi>
2021-06-24 17:19:32 +0300

    syntax changes in 0.14.9

--- vsphere/variables.tf
+++ vsphere/variables.tf
@@ -2,18 +2,18 @@
 
 variable "vsphere_server" {
   description = "vsphere server info"
-  type = "string"
+  type = string
   default = "192.168.20.142"
 }
 
 variable "vsphere_user" {
   description = "administrative user"
-  type = "string"
+  type = string
   default = "root"
 }
 
 variable "vsphere_password" {
   description = "administrative user password"
-  type = "string"
+  type = string
   default = "YourPasswordH3r3"
 }
