3bf94080eefee8c8e9e1432bfe5a3759420963a4
Khabir Uddin <khuddin@csc.fi>
2021-06-29 00:53:24 +0300

    variables

--- vsphere/variables.tf
+++ vsphere/variables.tf
@@ -18,9 +18,54 @@ variable "vsphere_password" {
   default = "YourPasswordH3r3"
 }
 
+variable "datacenter" {
+  description = "datacenter name"
+  type = string
+  default = "HomeDataCenter"
+}
+
+variable "datastore" {
+  description = "datastore name"
+  type = string
+  default = "datastore1"
+}
+
+variable "pubnet" {
+  description = "Public network"
+  type = string
+  default = "VMNetwork"
+}
+
+variable "internal" {
+  description = "Internal network"
+  type = string
+  default = "InterNal1"
+}
+
+variable "pool" {
+  description = "Resource pool"
+  type = string
+  default = "resourcePool1"
+}
+
+variable "ubuntu18template" {
+  description = "Ubuntu18 template" 
+  default = "ubuntu18template"
+}
+
+variable "ubuntu16template" {
+  description = "Ubuntu16 template" 
+  default = "ubuntu16template"
+}
+
 variable "vmname" {
   description = "Name of VM"
   default = "vmname"
+}
+
+variable "vnictype" {
+  description = "Type of VNIX"
+  default = "vmxnet3"
 }
 
 variable "disk_size" {
@@ -47,16 +92,16 @@ variable "ram_size" {
   default = 1560
 }
 
+variable "domain" {
+  description = "domain name" 
+  type = string
+  default = "masum.io"
+}
+
 variable "ipv4mask" {
   description = "ipv4 Subnet mask"
   type = string
   default = 24
-}
-
-variable "ipaddress" {
-   description = "VM IP address"
-   type = list
-   default = ["192.168.20.200"]
 }
 
 variable "vmgateway" {
