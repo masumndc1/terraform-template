27e0a49f56d65d7b865bd4e7c5f6f2c625539a03
Khabir Uddin <khuddin@csc.fi>
2022-01-24 19:08:27 +0200

    added internal network

--- openstack/main.tf
+++ openstack/main.tf
@@ -30,11 +30,29 @@ provider "openstack" {
   # like, source source.sh
 }
 
+# Internal network
+resource "openstack_networking_network_v2" "project_internal_network" {
+  name           = "${var.project_internal_network}"
+  admin_state_up = "true"
+}
+
+# Internal network subnet 1
+resource "openstack_networking_subnet_v2" "subnet_1" {
+  network_id = "${openstack_networking_network_v2.var.project_internal_network.id}"
+  cidr       = "${var.internal_network_block}"
+  ip_version = 4
+}
+
 # router of the project
 resource "openstack_networking_router_v2" "primary-router" {
   name                = "${var.project_main_router}"
   admin_state_up      = true
   external_network_id = "${data.openstack_networking_network_v2.public.id}"
+}
+
+resource "openstack_networking_router_interface_v2" "router_internal_interface_1" {
+  router_id = "${openstack_networking_router_v2.primary-router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.subnet_1.id}"
 }
 
 #module "centos" {
