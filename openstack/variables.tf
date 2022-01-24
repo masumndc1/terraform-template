73b336b129e44350337eb97f60be8e73276d2374
Khabir Uddin <khuddin@csc.fi>
2022-01-24 18:49:06 +0200

    more network info

--- openstack/variables.tf
+++ openstack/variables.tf
@@ -6,15 +6,20 @@ variable "project" {
   default = ""
 }
 
+variable "project_main_router" {
+  description = "project primary router"
+  type = string
+  default = "primary-router"
+}
+
 variable "project_external_network" {
   description = "project public network info"
   type = string
   default = "public"
 }
 
-variable "project_main_router" {
-  description = "project public network info"
+variable "project_internal_network" {
+  description = "project internal network info"
   type = string
-  default = "primary-router"
+  default = "internal"
 }
-
