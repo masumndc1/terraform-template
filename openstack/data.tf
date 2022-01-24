4c4cd8459f41a6c488829ce71c2dc6afb6ba6f45
Khabir Uddin <khuddin@csc.fi>
2022-01-24 18:42:33 +0200

    all data will be passed from here

---
+++ openstack/data.tf
@@ -0,0 +1,12 @@
+# all the data sources are from here
+#
+
+# get the info on project
+#data "openstack_identity_project_v3" "project" { 
+#  name = "${var.project}"
+#}
+
+
+data "openstack_networking_network_v2" "public" {
+  name = "${var.project_external_network}"
+}
