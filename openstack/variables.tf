c3c7024418fc4ed72bffcbf4feacd739235e2ca8
Khabir Uddin <khuddin@csc.fi>
2022-01-24 19:27:55 +0200

    image related info

--- openstack/variables.tf
+++ openstack/variables.tf
@@ -29,3 +29,27 @@ variable "internal_network_block" {
   type = string
   default = "192.168.50.0/24"
 }
+
+variable "image_centos7" {
+  description = "centos 7 image"
+  type = string
+  default = "CentOS-7"
+}
+
+variable "image_centos8" {
+  description = "centos 8 image"
+  type = string
+  default = "CentOS-8"
+}
+
+variable "image_ubuntu18" {
+  description = "ubuntu 18.04 image"
+  type = string
+  default = "Ubuntu-18.04"
+}
+
+variable "image_ubuntu20" {
+  description = "ubuntu 20.04 image"
+  type = string
+  default = "Ubuntu-20.04"
+}
