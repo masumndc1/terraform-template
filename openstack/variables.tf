f6a184bd178f31659c7d4010cf09fa93ed8f96d3
Khabir Uddin <khuddin@csc.fi>
2022-01-24 18:42:18 +0200

    all variables are here

--- openstack/variables.tf
+++ openstack/variables.tf
@@ -1,1 +1,20 @@
 # variables will come here
+
+variable "project" {
+  description = "openstack project info"
+  type = string
+  default = ""
+}
+
+variable "project_external_network" {
+  description = "project public network info"
+  type = string
+  default = "public"
+}
+
+variable "project_main_router" {
+  description = "project public network info"
+  type = string
+  default = "primary-router"
+}
+
